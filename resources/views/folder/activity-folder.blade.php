823a847605c5185f29d91899ad92fcd3021b392d
syarbinilucky <syarbinilucky@gmail.com>
2022-12-06 11:37:30 +0800

    update activity

--- resources/views/folder/activity-folder.blade.php
+++ resources/views/folder/activity-folder.blade.php
@@ -1,26 +1,32 @@
 <div class="tab-content" id="side">
     <div class="tab-pane fade show active tab-custome" role="tabpanel">
         <div class="card-body d-flex align-content-center">
-            <x-heroicon-s-folder-open style="width:15px" />
+            <x-heroicon-s-folder-open style="width:20px" />
             <p class="my-auto pl-2">{{ $folder->name }}</p>
         </div>
         <div class="detail">
             <div class="col-12 p-0">
                 <div class="d-flex justify-content-left p-0">
-                    <select name="" id="">
-                        <option value="">xx</option>
-                    </select>
+                    {{-- <select  name="" id="">
+                        <option value="">Today</option>
+                    </select> --}}
+                    <div class=" dropdown">
+                        <a class="dropdown-toggle">
+                          Today
+                        </a>
+                    </div>
                 </div>
                 @foreach($folder_activity as $data)
-
                 <div>
                     <div class="d-flex justify-content-between">
-                        <div>{{ $data->created_at }}</div>
+                        <div>{{ $data->created_at->format('H:i:s') }}</div>
                         <div>{{ $data->description }}</div>
                     </div>
                     <div>{{ $data->causer->name }}</div>
                     <div class="d-flex">
-                        <div>logo</div>
+                        <div class="d-flex justify-content-beetween">
+                          <x-heroicon-s-document style="width:18px"/>
+                        </div>
                         <div>{{ $data->subject->name }}</div>
                     </div>
                 </div>
